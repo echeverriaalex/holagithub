efc942cae40600ccaa85c66b557bd8d47a99ccf1
echeverriaalex <aleex.naahuel@outlook.com>
2020-06-10 16:22:19 -0300

    creo un persona y la muestro en main

--- Prueba con github/src/Main.java
+++ Prueba con github/src/Main.java
@@ -4,5 +4,9 @@ public class Main {
 	public static void main(String[] args) {
 		
 		System.out.println("hola github");
+		
+		Persona persona = new Persona("Alex", "Echeverria");
+		System.out.println(persona.toString());
+		System.out.println("ya la clone y ahora voy a actualizarlo en github");
 	}
 }
